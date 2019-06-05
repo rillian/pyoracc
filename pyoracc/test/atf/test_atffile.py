8642b18b1d7b5956eb3c746a01ef448da052fa84
Ralph Giles <giles@thaumas.net>
2019-06-05 15:44:38 -0700

    Add a test for AtfFile.to_json().
    Run the sample_corpus files through the AtfFile and then
    the json serializer. Check that they're valid json, but
    not much else.

--- pyoracc/test/atf/test_atffile.py
+++ pyoracc/test/atf/test_atffile.py
@@ -20,6 +20,8 @@ along with PyORACC. If not, see <http://www.gnu.org/licenses/>.
 
 from pyoracc.atf.common.atffile import AtfFile
 from ..fixtures import anzu, belsunu, sample_file
+import pytest
+import json
 
 
 def test_create():
@@ -121,3 +123,18 @@ def test_composites():
     """
     for composite in composites:
         yield consider_composite, composite[0], composite[1]
+
+
+@pytest.mark.parametrize('name', [text[0] for text in texts])
+def test_json_serialization(name):
+    """
+    Parses ATF and verifies the to_json() method output.
+    """
+    afile = AtfFile(sample_file(name))
+    js = afile.to_json()
+    result = json.loads(js)
+    assert result
+    noskipjs = afile.to_json(skip_empty=False)
+    result = json.loads(noskipjs)
+    assert result
+    assert len(noskipjs) >= len(js)
